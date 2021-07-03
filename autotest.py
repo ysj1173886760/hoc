8224fbfda4b7cfa22af36b72a3bc05fb03988ea4
ysj1173886760 <1173886760@qq.com>
2021-07-03 19:11:38 +0800

    upload auto test boolstrap and answers for test

---
+++ autotest.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+import sys
+
+class Tester:
+
+    testFileDirectory = "./test"
+
+    testList = [
+        "field", 
+        "for", 
+        "gcd",
+        "read",
+        "test1",
+        "test2",
+        "test3",
+        "test4",
+    ]
+
+    procname = "./hoc"
+
+    def runTest(self, testName):
+        filename = os.path.join(self.testFileDirectory, "{}.test".format(testName))
+        procArgs = self.procname + " " + filename
+        try:
+            p = subprocess.Popen(procArgs, shell=True, stdout=subprocess.PIPE)
+            stdout_data = p.communicate()[0]
+        except Exception as e:
+            print("Call of '{}' failed: {}".format(procArgs, e))
+            return
+        
+        resname = os.path.join(self.testFileDirectory, "{}.res".format(testName))
+        res = open(resname, "rb")
+        res_data = res.read()
+        if (res_data == stdout_data):
+            print("{} test passed".format(testName))
+        else:
+            print("{} test failed \ngot:\n{}\nexpecting:\n{}\n".format(testName, stdout_data.decode("utf-8"), res_data.decode("utf-8")))
+    
+    def run(self):
+        for testName in self.testList:
+            self.runTest(testName)
+
+
+t = Tester()
+t.run()
